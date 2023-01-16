7d7c18c1f19042f5531443b2260798d210bebf83
LamiaZaghloulOmar <lamiaa.zaghloul258@gmail.com>
2023-01-16 16:36:19 +0200

    updates

--- android/app/src/main/kotlin/app/naqeel/naqeel_delivery/LifecycleDetector.kt
+++ android/app/src/main/kotlin/app/naqeel/naqeel_delivery/LifecycleDetector.kt
@@ -1,4 +1,4 @@
-package com.souq.souq_delivery
+package com.souq.souqapp_delivery
 
 import android.app.Activity
 import android.app.Application
--- android/app/src/main/kotlin/com/naqeel/naqeel_delivery/MainActivity.kt
+++ android/app/src/main/kotlin/com/naqeel/naqeel_delivery/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.souq.souq_delivery
+package com.souq.souqapp_delivery
 
 import android.content.Intent
 import android.os.Bundle
